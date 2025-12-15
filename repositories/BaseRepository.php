0a39fe02001f39f4841f95472a61e858b7618a50
idaballegaard <idabal01@easv365.dk>
2025-12-15 20:59:09 +0100

    Update BaseRepository.php

--- repositories/BaseRepository.php
+++ repositories/BaseRepository.php
@@ -1,13 +1,23 @@
 <?php
 require_once __DIR__ . "/UserRepository.php";
 abstract class BaseRepository {
+
+    private array $data = [];
+
+    public function __construct() {
+      $this->data = parse_ini_file(__DIR__ . "/../.env");
+    }
+
+    protected function getEnvVariable(string $key): ?string {
+      return $this->data[$key] ?? null;
+    }
     protected function connectDatabase() {
-        $db_name = "mysql:dbname=RetroBioDBNEW";
-        $db_host = "host=localhost";
+        $db_name = $this->getEnvVariable("DB_NAME");
+        $db_host = $this->getEnvVariable("DB_HOST");
         $db_charset = "charset=utf8";
         $dsn = "$db_name; $db_host; $db_charset";
-        $dbUser = "IdaB";
-        $dbPass = "123456";
+        $dbUser = $this->getEnvVariable("DB_USER");
+        $dbPass = $this->getEnvVariable("DB_PASS");
 
         try{
             return new PDO($dsn, $dbUser, $dbPass);}
